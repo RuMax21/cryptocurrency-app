f2c6431ae44aab31af3d504df1e9c726599b0fe1
RuMax21 <cherednikov.maxim@mail.ru>
2026-03-11 23:22:10 +0300

    create a Coin entity: hook & api

---
+++ src/entities/Coin/model/useCrypto.ts
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { DEFAULT_CRYPTO, REFETCH_INTERVAL } from "./constants"
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getPrices } from "../api";
+
+export const useCrypto = () => {
+  const [coinIds, setCoinIds] = useState<string[]>([DEFAULT_CRYPTO]);
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['prices', coinIds],
+    queryFn: () => getPrices(coinIds),
+    refetchInterval: REFETCH_INTERVAL,
+  });
+
+  const addCoin = (id: string) => {
+    const normalized = id.toUpperCase();
+    if (!coinIds.includes(normalized)) {
+      setCoinIds(prev => [...prev, normalized]);
+    }
+  }
+
+  const deleteCoin = (id: string) => {
+    setCoinIds(prev => prev.filter(item => item !== id));
+  }
+
+  const updateAll = () => {
+    queryClient.invalidateQueries({ queryKey: ['prices'] });
+  }
+
+  const updateOne = (id: string) => {
+    queryClient.invalidateQueries({ queryKey: ['prices', id] })
+  }
+
+  return { data, isLoading, isError, addCoin, deleteCoin, updateAll, updateOne };
+}
--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,1 +1,1 @@
-export { cryptoApi } from './cryptoInstance';
+export { cryptoApi } from './cryptoApi';
